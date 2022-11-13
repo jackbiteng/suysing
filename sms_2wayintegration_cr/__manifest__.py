7911716c62d960d3b38b437655ba3939a58ca072
Aarav SJ <99322833+aaravsj@users.noreply.github.com>
2022-11-13 17:33:30 +0530

    sms Integration

---
+++ sms_2wayintegration_cr/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo Module Developed by Candidroot Solutions Pvt. Ltd.
+# See LICENSE file for full copyright and licensing details.
+{
+    'name': "SMS 2-way integration",
+    'category': 'Extra Tools',
+    'summary': """Send sms 2 way integration.""",
+    'version': '15.0.0.1',
+    'author': "Candidroot Solutions Pvt. Ltd.",
+    'website': 'https://www.candidroot.com/',
+    'sequence': 2,
+    'description': """This module allows you to send sms using api""",
+    'depends': ["marketing_automation", "sms"],
+    'data': [
+        'views/res_config_settings_views.xml',
+    ],
+    'qweb': [],
+    'images' : ['static/description/banner.png'],
+    'installable': True,
+    'live_test_url': 'https://youtu.be/XTBuZzlva3I',
+    'price': 9.99,
+    'currency': 'USD',
+    'auto_install': False,
+    'application': False,
+}
