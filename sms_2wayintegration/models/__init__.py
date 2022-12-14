14711d95ad9704382f7b085ac4dc96984eb20ca9
erp <erp>
2022-12-14 17:14:11 +0530

    [ADD] Fix issue

--- sms_2wayintegration/models/__init__.py
+++ sms_2wayintegration/models/__init__.py
@@ -4,3 +4,4 @@
 
 from . import res_config_settings
 from . import sms_api
+from . import sms_composer
--- sms_2wayintegration/models/sms_api.py
+++ sms_2wayintegration/models/sms_api.py
@@ -3,27 +3,28 @@
 
 from odoo.addons.iap.tools import iap_tools
 DEFAULT_ENDPOINT = 'https://iap.odoo.com'
-
+import requests
 
 def iap_jsonrpc(url, method='call', params=None, timeout=15):
-    import requests
     from odoo.http import request
-
     user = request.env['ir.config_parameter'].sudo().get_param('sms_2wayintegration.msegat_user_name')
     password = request.env['ir.config_parameter'].sudo().get_param('sms_2wayintegration.msegat_api_key')
     originator = request.env['ir.config_parameter'].sudo().get_param('sms_2wayintegration.msegat_user_sender')
     result=[]
-    for p in params.get('messages', False):
-        url = "https://svr22.synermaxx.asia/vmobile/suysing/api/sendnow.php?username=%s&password=%s&mobilenum=%s&fullmesg=%s&originator=%s" %(
-            user, password,
-            p.get('number',False),
-            p.get('content',False),
-            originator)
-        request = requests.get(url)
-        if request.status_code == 200:
-            result.append({'state': 'success', 'credit': 0, 'res_id': p.get('res_id', False)})
-        else:
-            result.append({'state': 'unregistered', 'credit': 0, 'res_id': p.get('res_id', False)})
+    if params and params.get('messages', False):
+        for p in params.get('messages'):
+            url = "https://svr22.synermaxx.asia/vmobile/suysing/api/sendnow.php?username=%s&password=%s&mobilenum=%s&fullmesg=%s&originator=%s" %(
+                user, password,
+                p.get('number',False),
+                p.get('content',False),
+                originator)
+            import pdb
+            pdb.set_trace()
+            response = requests.get(url)
+            if response.status_code == 200:
+                result.append({'state': 'success', 'credit': 0, 'res_id': p.get('res_id', False)})
+            else:
+                result.append({'state': 'unregistered', 'credit': 0, 'res_id': p.get('res_id', False)})
     return result
 
 iap_tools.iap_jsonrpc = iap_jsonrpc
