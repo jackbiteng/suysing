9edbf3370a29f6a9dc68b6ff114c506540efaa5d
erp <erp>
2022-12-16 18:07:44 +0530

    [IMP] Fix url encodeing issue

--- sms_2wayintegration/models/sms_api.py
+++ sms_2wayintegration/models/sms_api.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
+import werkzeug
 from odoo.addons.iap.tools import iap_tools
 DEFAULT_ENDPOINT = 'https://iap.odoo.com'
 import requests
@@ -13,11 +13,14 @@ def iap_jsonrpc(url, method='call', params=None, timeout=15):
     result=[]
     if params and params.get('messages', False):
         for p in params.get('messages'):
-            url = "https://svr22.synermaxx.asia/vmobile/suysing/api/sendnow.php?username=%s&password=%s&mobilenum=%s&originator=%s&fullmesg=%s" %(
-                user, password,
-                p.get('number',False),
-                originator,
-                p.get('content',False))
+            params_json = {
+                "username": user,
+                "password": password,
+                "mobilenum": p.get('number',False),
+                "originator": originator,
+                "fullmesg": p.get('content',False)
+            }
+            url = "https://svr22.synermaxx.asia/vmobile/suysing/api/sendnow.php?"+werkzeug.urls.url_encode(params_json)
             response = requests.get(url)
             if not response.text.split('|')[0].endswith('NACK'):
                 result.append({'state': 'success', 'credit': 0, 'res_id': p.get('res_id', False)})
